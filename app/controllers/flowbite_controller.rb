f359917da3dca1a279cfe0948d4a96d47d7d94a9
Thomas Breier <breierthomas@gmail.com>
2024-07-22 17:03:19 +0200

    simple POC

--- app/controllers/flowbite_controller.rb
+++ app/controllers/flowbite_controller.rb
@@ -2,4 +2,15 @@ class FlowbiteController < ApplicationController
 
   def index
   end
+
+  def update
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace(
+          'main', partial: "flowbite/tooltip"
+        )
+      end
+    end
+  end
+
 end
